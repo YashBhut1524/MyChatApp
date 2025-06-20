69aa3a80abfdc0f966f2a8fbb441f8245e47b31b
YashBhut1524 <yashxerox15@gmail.com>
2025-06-20 23:00:39 +0530

    udate path

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -2,5 +2,5 @@ import axios from "axios"
 import { HOST } from "@/utils/constans.js"
 
 export const apiClient = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? HOST : "/"
+    baseURL: import.meta.env.MODE === "development" ? HOST : window.location.href,
 })
