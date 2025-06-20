12844c4f74c404490e912a621d2cd488f9b5d9d0
YashBhut1524 <yashxerox15@gmail.com>
2025-06-20 23:13:45 +0530

    hardcoded path

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -2,5 +2,6 @@ import axios from "axios"
 import { HOST } from "@/utils/constans.js"
 
 export const apiClient = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? HOST : "https://mychatapp-47h3.onrender.com",
+    baseURL: "https://mychatapp-47h3.onrender.com",
+    // baseURL: import.meta.env.MODE === "development" ? HOST : "https://mychatapp-47h3.onrender.com",
 })
